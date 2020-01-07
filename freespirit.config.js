3fe90434d7e9b9efb1f9019010789ab5763e2f08
SirReiva <gjjavicoru@hotmail.com>
2020-01-08 00:53:23 +0100

    merge configs

--- freespirit.config.js
+++ freespirit.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    "port": "80",
+    "port": 8080,
     "default_readFile": "index.php",
     "php_Path": "C:/wamp64/bin/php/php7.2.10/php.exe",
     "htdocs": path.join(__dirname, '/htdocs'),
--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const Compiler = require('../compiler');
 const fs = require('fs');
-let Default_readFile = 'index.php';
 const os = require("os");
 const cluster = require("cluster");
 const configFile = require('../../freespirit.config.js');
 const path = require('path');
+const DEFAULT_CONFIG = {
+    "port": 80,
+    "default_readFile": "index.php",
+    "php_Path": "C:/wamp64/bin/php/php7.2.10/php.exe",
+    "htdocs": path.join(__dirname, '/htdocs'),
+    "max_cpus": 2
+};
 
 class FreeSpiritServer {
     constructor() {
+        this.config = Object.assign({}, DEFAULT_CONFIG, configFile);
         this.app = express();
-        this.port = configFile.port;
-        console.log(configFile.htdocs);
-        this.currentDir = configFile.htdocs;
         const CPUS = Math.min(os.cpus().length, configFile.max_cpus);
         if (CPUS > 1) {
             switch (cluster.isMaster) {
@@ -24,7 +28,7 @@ class FreeSpiritServer {
                     |   __|  _| -_| -_|__   | . | |  _|  _|
                     |__|  |_| |___|___|_____|  _|_|_| |_|
                                             |_|            `); //cyan
-                    console.log({ cpus: CPUS, port: this.port });
+                    console.log({ cpus: CPUS, port: this.config.port });
                     for (let i = 0; i < CPUS; i++) { //Forge New process for Cpu
                         cluster.fork();
                     }
@@ -43,22 +47,22 @@ class FreeSpiritServer {
 
     init() {
         this.app.get('*', (req, res) => {
-            var Directory = req.params[0].toString();
-            console.log(Directory)
-            fs.stat(path.join(this.currentDir, Directory), (err, stats) => {
+            let pathReq = req.params[0].toString();
+            console.log(pathReq)
+            fs.stat(path.join(this.config.htdocs, pathReq), (err, stats) => {
                 if (err) {
                     this.sendInternalServerError(req, res);
                 } else if (stats.isDirectory()) {
                     this.sendDefaultFile(req, res);
                 } else if (stats.isFile()) {
-                    this.sendFile(req, res, Directory, stats);
+                    this.sendFile(req, res, pathReq);
                 } else {
                     console.log('unkwon type');
                 }
             });
 
         });
-        this.app.listen(this.port, () => console.log(`Listen ${this.port}`))
+        this.app.listen(this.config.port, () => console.log(`Listen ${this.config.port}`));
     }
 
     send404(req, res) {
@@ -66,12 +70,12 @@ class FreeSpiritServer {
     }
 
     async sendDefaultFile(req, res) {
-        fs.stat(path.join(this.currentDir, Default_readFile), async(err, stats) => {
+        fs.stat(path.join(this.config.htdocs, this.config.default_readFile), async(err, stats) => {
             try {
                 if (err) {
                     this.sendInternalServerError(req, res);
                 } else if (stats.isFile()) {
-                    let response_complie = await this.open(res, Default_readFile);
+                    let response_complie = await this.open(res, this.config.default_readFile);
                     res.send(response_complie);
                 } else {
                     this.send404(req, res);
@@ -82,7 +86,7 @@ class FreeSpiritServer {
         });
     }
 
-    async sendFile(req, res, Directory, stats) {
+    async sendFile(req, res, Directory) {
         try {
             let response_complie = await this.open(res, Directory);
             res.send(response_complie);
@@ -93,7 +97,7 @@ class FreeSpiritServer {
 
     async open(res, filePath) {
         let extension = path.extname(filePath).substr(1);
-        let response_complie = await Compiler.compile(res, extension, filePath, this.currentDir);
+        let response_complie = await Compiler.compile(res, extension, filePath, this.config.htdocs);
         return response_complie;
     }
 
